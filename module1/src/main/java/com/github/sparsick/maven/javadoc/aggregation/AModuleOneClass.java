c5ce4ec20b3de358b60fd20610a9ab005d0b5e0e
sparsick <sparsick@web.de>
2016-12-22 19:43:07 +0100

    sample classes

---
+++ module1/src/main/java/com/github/sparsick/maven/javadoc/aggregation/AModuleOneClass.java
@@ -0,0 +1,16 @@
+package com.github.sparsick.maven.javadoc.aggregation;
+
+/**
+ * Little bit javadoc
+ * 
+ */
+public class AModuleOneClass {
+    
+    /**
+     *  a little bit javadoc
+     */
+    public void aMethod() {
+        
+    }
+
+}
---
+++ module2/src/main/java/com/github/sparsick/maven/javadoc/aggregation/AModuleTwoClass.java
@@ -0,0 +1,18 @@
+package com.github.sparsick.maven.javadoc.aggregation;
+
+/**
+ * foo javadoc
+ *
+ */
+public class AModuleTwoClass {
+
+    /**
+     *  bar javadoc
+     * 
+     * @return String value
+     */
+    public String methodWithReturnValue() {
+        return "";
+    }
+
+}
